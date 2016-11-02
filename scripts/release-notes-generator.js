1fe643d8c64bf2b7012cee4e454041c233c24d1d
Artem Sapegin <artem@sapegin.ru>
2016-11-02 22:02:23 +0100

    Chore: Correct formatting of Markdown lists.

--- scripts/release-notes-generator.js
+++ scripts/release-notes-generator.js
@@ -1,6 +1,7 @@
 const SRError = require('@semantic-release/error');
 const gitLatestSemverTag = require('git-latest-semver-tag');
 const commitsBetween = require('commits-between');
+const indent = require('indent');
 const isChangelog = require('./util').isChangelog;
 
 const removeHeader = text => text.substring(text.indexOf('\n') + 1).trim();
@@ -9,7 +10,9 @@ const isFix = commit => commit.startsWith('Fix:');
 
 const highlightTag = text => text.replace(/^Fix:/, '**Fixed:**');
 
-const asList = items => '* ' + items.join('\n* ');
+const formatListItem = text => '* ' + indent(text, 2).trim();
+
+const asList = items => items.map(formatListItem).join('\n');
 
 module.exports = (pluginConfig, config, cb) => {
 	gitLatestSemverTag((err, tag) => {
